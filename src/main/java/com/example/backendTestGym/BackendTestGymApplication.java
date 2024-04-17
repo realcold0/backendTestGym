8c5885dec16b134d0365ad6f1009b378f09a9d54
realcold0 <realcold0@gmail.com>
2024-04-17 23:25:25 +0900

    Initial commit

---
+++ src/main/java/com/example/backendTestGym/BackendTestGymApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendTestGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTestGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendTestGym/BackendTestGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendTestGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
